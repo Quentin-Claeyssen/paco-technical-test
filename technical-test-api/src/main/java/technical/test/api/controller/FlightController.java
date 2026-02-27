e513382e6d857300a18cbc3427d29eff771ccbcd
Quentin-Claeyssen <quentin.claeyssen.partner@decathlon.com>
2026-02-27 17:42:12 +0100

    feat(Facade): Remove

--- technical-test-api/src/main/java/technical/test/api/controller/FlightController.java
+++ technical-test-api/src/main/java/technical/test/api/controller/FlightController.java
@@ -5,17 +5,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
-import technical.test.api.facade.FlightFacade;
 import technical.test.api.dto.output.FlightOut;
+import technical.test.api.services.FlightService;
 
 @RestController
 @RequestMapping("/flight")
 @RequiredArgsConstructor
 public class FlightController {
-    private final FlightFacade flightFacade;
+    private final FlightService flightService;
 
     @GetMapping
     public Flux<FlightOut> getAllFlights() {
-        return flightFacade.getAllFlights();
+        return flightService.getAllFlights();
     }
 }
--- technical-test-api/src/main/java/technical/test/api/services/FlightService.java
+++ technical-test-api/src/main/java/technical/test/api/services/FlightService.java
@@ -3,16 +3,33 @@ package technical.test.api.services;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
-import technical.test.api.entity.Flight;
+import reactor.core.publisher.Mono;
+import technical.test.api.dto.output.FlightOut;
+import technical.test.api.entity.Airport;
+import technical.test.api.mapper.AirportMapper;
+import technical.test.api.mapper.FlightMapper;
 import technical.test.api.repository.FlightRepository;
 
 @Service
 @RequiredArgsConstructor
 public class FlightService {
     private final FlightRepository flightRepository;
+    private final AirportService airportService;
+    private final FlightMapper flightMapper;
+    private final AirportMapper airportMapper;
 
-    public Flux<Flight> getAllFlights() {
-        return flightRepository.findAll();
+    public Flux<FlightOut> getAllFlights() {
+        return flightRepository.findAll()
+                .flatMap(flightRecord -> airportService.findByIataCode(flightRecord.getOrigin())
+                        .zipWith(airportService.findByIataCode(flightRecord.getDestination()))
+                        .flatMap(tuple -> {
+                            Airport origin = tuple.getT1();
+                            Airport destination = tuple.getT2();
+                            FlightOut flightOut = this.flightMapper.convert(flightRecord);
+                            flightOut.setOrigin(this.airportMapper.convert(origin));
+                            flightOut.setDestination(this.airportMapper.convert(destination));
+                            return Mono.just(flightOut);
+                        }));
     }
 
 }
