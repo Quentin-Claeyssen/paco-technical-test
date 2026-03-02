86ef6d62d1e951ab8b3e4b985f2eea74e2acb7c6
Quentin-Claeyssen <quentin.claeyssen.partner@decathlon.com>
2026-03-02 10:49:56 +0100

    feat(Flight): Add sort by price

--- technical-test-api/src/main/java/technical/test/api/controller/FlightController.java
+++ technical-test-api/src/main/java/technical/test/api/controller/FlightController.java
@@ -3,6 +3,7 @@ package technical.test.api.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -17,8 +18,9 @@ public class FlightController {
     private final FlightService flightService;
 
     @GetMapping
-    public Flux<FlightOut> getAll(@RequestParam(defaultValue = "0") int page) {
-        Pageable pageable = PageRequest.of(page, 6);
+    public Flux<FlightOut> getAll(@RequestParam(defaultValue = "0") int page,
+                                  @RequestParam(defaultValue = "price") String sortBy) {
+        Pageable pageable = PageRequest.of(page, 6, Sort.by(sortBy).ascending());
         return flightService.getAll(pageable);
     }
 
